d463d0faa21d997f117e1873b88e01f52158fc86
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-27 18:32:13 +0100

    Show some fake data on the SalesActivity.

---
+++ app/src/main/java/com/example/ismailamrani/comptable/Adapters/SoldProductAdapter.java
@@ -0,0 +1,53 @@
+package com.example.ismailamrani.comptable.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.ismailamrani.comptable.R;
+
+import java.util.List;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 27/04/2016.
+ */
+public class SoldProductAdapter extends ArrayAdapter<String> {
+
+    private Context context;
+    private List<String> products;
+
+    public SoldProductAdapter(Context context, List<String> products) {
+        super(context, -1, products);
+        this.context = context;
+        this.products = products;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            // inflate the layout
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(R.layout.sold_product_row, parent, false);
+            // Setup the ViewHolder
+            viewHolder = new ViewHolder();
+            viewHolder.productText = (TextView) convertView.findViewById(R.id.productText);
+            // store the holder with the view.
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.productText.setText(products.get(position));
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView productText;
+    }
+}
--- app/src/main/java/com/example/ismailamrani/comptable/ui/SalesActivity.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/SalesActivity.java
@@ -2,15 +2,31 @@ package com.example.ismailamrani.comptable.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.ismailamrani.comptable.Adapters.SoldProductAdapter;
 import com.example.ismailamrani.comptable.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class SalesActivity extends Activity {
+
+    @Bind(R.id.productsListview)
+    ListView productsListview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sales_layout);
+        ButterKnife.bind(this);
+
+        List<String> data = Arrays.asList("IPhone 6 Plus", "Samsung Galaxy S7",
+                "Samsung Trend Plus", "IPhone 5S");
+        productsListview.setAdapter(new SoldProductAdapter(this, data));
     }
 }
