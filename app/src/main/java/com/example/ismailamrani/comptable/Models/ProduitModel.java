d7caa448c6fce33ed5ac0d18585b6736a2d00863
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-28 17:01:33 +0100

    Add an overloaded constructor to the Product's model class that creates a new instance extracted from a JSON object.

--- app/src/main/java/com/example/ismailamrani/comptable/Models/ProduitModel.java
+++ app/src/main/java/com/example/ismailamrani/comptable/Models/ProduitModel.java
@@ -1,5 +1,8 @@
 package com.example.ismailamrani.comptable.Models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Ismail Amrani on 23/03/2016.
  */
@@ -14,6 +17,39 @@ public class ProduitModel {
     int Qte;
     int Locale_ID;
     String url;
+
+    /**
+     * Overloaded constructor.
+     * Creates a new Product model based on the passed-in JSON object
+     *
+     * @param object to be parsed.
+     */
+    public ProduitModel(JSONObject object) throws JSONException {
+        this(
+                object.getInt("ID"),
+                object.getString("Libelle"),
+                object.getDouble("PrixHT"),
+                object.getDouble("PrixTTC"),
+                object.getString("CodeBar"),
+                object.getString("Photo"),
+                object.getInt("Qte"),
+                object.getInt("Locale_ID"),
+                ""
+        );
+    }
+
+    public ProduitModel(int ID, String libelle, Double prixHT, Double prixTTC,
+                        String codeBarre, String photo, int qte, int locale_ID, String url) {
+        this.ID = ID;
+        Libelle = libelle;
+        PrixHT = prixHT;
+        PrixTTC = prixTTC;
+        CodeBarre = codeBarre;
+        Photo = photo;
+        Qte = qte;
+        Locale_ID = locale_ID;
+        this.url = url;
+    }
 
     public String getUrl() {
         return url;
