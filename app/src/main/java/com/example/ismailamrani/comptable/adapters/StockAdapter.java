288e0e5e39a4a41629508853dad95a20db1ef7cf
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-01 10:53:09 +0100

    Apply a blur transformation on the stock's products image.

--- app/src/main/java/com/example/ismailamrani/comptable/adapters/StockAdapter.java
+++ app/src/main/java/com/example/ismailamrani/comptable/adapters/StockAdapter.java
@@ -5,13 +5,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.ismailamrani.comptable.R;
 import com.example.ismailamrani.comptable.models.Product;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jp.wasabeef.picasso.transformations.BlurTransformation;
 
 /**
  * Created by Mohammed Aouf ZOUAG on 30/04/2016.
@@ -49,16 +53,22 @@ public class StockAdapter extends RecyclerView.Adapter<StockAdapter.ViewHolder> 
 
         TextView productLabel;
         TextView quantityLabel;
+        ImageView productImage;
 
         ViewHolder(View v) {
             super(v);
             productLabel = (TextView) v.findViewById(R.id.productLabel);
             quantityLabel = (TextView) v.findViewById(R.id.quantityLabel);
+            productImage = (ImageView) v.findViewById(R.id.productImage);
         }
 
         public void bind(Product product) {
             productLabel.setText(product.getLibelle());
             quantityLabel.setText(String.valueOf(product.getQte()));
+            Picasso.with(itemView.getContext())
+                    .load(R.drawable.iphone)
+                    .transform(new BlurTransformation(itemView.getContext()))
+                    .into(productImage);
         }
     }
 }
