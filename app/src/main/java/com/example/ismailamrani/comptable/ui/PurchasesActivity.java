e5f7a58f2e4f351b3a38844e55aea28c74e11ea5
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-03 11:01:21 +0100

    PurchasesActivity: setup action bar.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/PurchasesActivity.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/PurchasesActivity.java
@@ -1,15 +1,37 @@
 package com.example.ismailamrani.comptable.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.ismailamrani.comptable.R;
+import com.example.ismailamrani.comptable.customitems.OGActionBar.OGActionBar;
+import com.example.ismailamrani.comptable.customitems.OGActionBar.OGActionBarInterface;
 
-public class PurchasesActivity extends AppCompatActivity {
+public class PurchasesActivity extends Activity implements OGActionBarInterface {
+
+    private OGActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchases);
+
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        mActionBar = (OGActionBar) findViewById(R.id.MyActionBar);
+        mActionBar.setActionBarListener(this);
+        mActionBar.setTitle("Achats");
+    }
+
+    @Override
+    public void onMenuPressed() {
+
+    }
+
+    @Override
+    public void onAddPressed() {
+
     }
 }
