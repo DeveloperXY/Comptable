73ca0756de05f7c75cddad9afbc23565f08277c1
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-10 09:23:18 +0100

    Create the layout of the 'PurchaseDialog'.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/dialogs/PurchaseDialog.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/dialogs/PurchaseDialog.java
@@ -2,6 +2,10 @@ package com.example.ismailamrani.comptable.ui.dialogs;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.ismailamrani.comptable.R;
 
 /**
  * Created by Mohammed Aouf ZOUAG on 10/05/2016.
@@ -9,5 +13,12 @@ import android.content.Context;
 public class PurchaseDialog extends Dialog {
     public PurchaseDialog(Context context) {
         super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.purchase_dialog);
     }
 }
