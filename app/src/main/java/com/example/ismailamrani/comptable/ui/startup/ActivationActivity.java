d5df47ebf9ec28127ad5fc3564e78f78bfb95130
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-10 17:03:03 +0100

    Create the ActivationActivity.

---
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/startup/ActivationActivity.java
@@ -0,0 +1,13 @@
+package com.example.ismailamrani.comptable.ui.startup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activation);
+    }
+}
