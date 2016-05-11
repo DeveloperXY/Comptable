870d0a863eb7f168fd34762a3eb7197bb3768097
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-11 14:37:22 +0100

    OrdersListFragment: bind UI elements with Butterknife.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/fragments/OrdersListFragment.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/fragments/OrdersListFragment.java
@@ -3,17 +3,52 @@ package com.example.ismailamrani.comptable.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.ismailamrani.comptable.R;
+import com.example.ismailamrani.comptable.customitems.OGActionBar.OGActionBar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OrdersListFragment extends Fragment {
 
+    /**
+     * The orders' list.
+     */
+    @Bind(R.id.recyclerView)
+    protected RecyclerView recyclerView;
+
+    @Bind(R.id.emptyMessageLabel)
+    protected TextView emptyMessageLabel;
+
+    @Bind(R.id.progressBar)
+    protected ProgressBar progressBar;
+
+    /**
+     * The view to be displayed in case a network error occur.
+     */
+    @Bind(R.id.errorLayout)
+    protected RelativeLayout errorLayout;
+
+    /**
+     * The view to be displayed in case there were no orders to show.
+     */
+    @Bind(R.id.emptyLayout)
+    protected RelativeLayout emptyView;
+
+    @Bind(R.id.swipeRefreshLayout)
+    protected SwipeRefreshLayout swipeRefreshLayout;
 
     public OrdersListFragment() {
         // Required empty public constructor
@@ -24,7 +59,11 @@ public class OrdersListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_orders_list, container, false);
+        ButterKnife.bind(this, view);
+
+
+        return view;
     }
 
 }
