730a16e78b95af75f8fc94b3ec8ebf39ce80ab38
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-11 14:29:52 +0100

    OrdersActivity: setup the action bar.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/orders/OrdersActivity.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/orders/OrdersActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.ismailamrani.comptable.R;
+import com.example.ismailamrani.comptable.customitems.OGActionBar.OGActionBar;
 import com.example.ismailamrani.comptable.ui.base.ColoredStatusBarActivity;
 import com.example.ismailamrani.comptable.utils.OrderEnum;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class OrdersActivity extends ColoredStatusBarActivity {
+
+    @Bind(R.id.MyActionBar)
+    protected OGActionBar mActionBar;
 
     private OrderEnum currentOrderType;
 
@@ -20,6 +25,12 @@ public class OrdersActivity extends ColoredStatusBarActivity {
         ButterKnife.bind(this);
 
         setupOrdersType();
+        setupActionBar();
+    }
+
+    protected void setupActionBar() {
+        mActionBar.setActionBarListener(this);
+        mActionBar.setTitle(currentOrderType == OrderEnum.PURCHASE ? "Commandes achats" : "Commandes ventes");
     }
 
     /**
@@ -34,5 +45,11 @@ public class OrdersActivity extends ColoredStatusBarActivity {
                 currentOrderType = "SALE".equals(incoming) ? OrderEnum.SALE : OrderEnum.PURCHASE;
             }
         }
+    }
+
+    @Override
+    public void onAddPressed() {
+        startActivity(new Intent(this, currentOrderType == OrderEnum.PURCHASE ?
+                PurchasesActivity.class : SalesActivity.class));
     }
 }
