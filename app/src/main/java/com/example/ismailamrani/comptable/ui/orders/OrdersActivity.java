86dce8888d514e077f6637f54168af2e2c4636aa
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-11 14:21:38 +0100

    OrdersActivity: code refactor.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/orders/OrdersActivity.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/orders/OrdersActivity.java
@@ -4,25 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.ismailamrani.comptable.R;
+import com.example.ismailamrani.comptable.ui.base.ColoredStatusBarActivity;
 import com.example.ismailamrani.comptable.utils.OrderEnum;
 
 import butterknife.ButterKnife;
 
-public class OrdersActivity extends AbstractOrdersActivity {
+public class OrdersActivity extends ColoredStatusBarActivity {
+
+    private OrderEnum currentOrderType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
         ButterKnife.bind(this);
 
+        setupOrdersType();
+    }
+
+    /**
+     * Retrieves the type of orders to be currently displayed.
+     */
+    private void setupOrdersType() {
         Intent intent = getIntent();
         if (intent != null) {
             Bundle data = intent.getExtras();
             if (data != null) {
                 String incoming = data.getString("orderType");
-                OrderEnum orderEnum = "SALE".equals(incoming) ? OrderEnum.SALE : OrderEnum.PURCHASE;
-
-
+                currentOrderType = "SALE".equals(incoming) ? OrderEnum.SALE : OrderEnum.PURCHASE;
             }
         }
     }
