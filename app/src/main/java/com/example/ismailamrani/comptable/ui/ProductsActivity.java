a862ece0fd15680324725f16cef46064117ee46c
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-21 17:02:22 +0100

    ProductsActivity: code refactor.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/ProductsActivity.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/ProductsActivity.java
@@ -1,34 +1,25 @@
 package com.example.ismailamrani.comptable.ui;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.ismailamrani.comptable.R;
 import com.example.ismailamrani.comptable.adapters.ProduitAdapter;
-import com.example.ismailamrani.comptable.customitems.OGActionBar.OGActionBar;
 import com.example.ismailamrani.comptable.models.Product;
 import com.example.ismailamrani.comptable.ui.base.ColoredStatusBarActivity;
+import com.example.ismailamrani.comptable.utils.Method;
+import com.example.ismailamrani.comptable.utils.RequestListener;
 import com.example.ismailamrani.comptable.webservice.PhpAPI;
-import com.example.ismailamrani.comptable.webservice.convertInputStreamToString;
-import com.example.ismailamrani.comptable.webservice.getQuery;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -36,7 +27,8 @@ import butterknife.ButterKnife;
  */
 public class ProductsActivity extends ColoredStatusBarActivity {
 
-    ListView List;
+    @Bind(R.id.List)
+    ListView productsListView;
     ArrayList<Product> ListProduit = new ArrayList<>();
 
     @Override
@@ -46,9 +38,7 @@ public class ProductsActivity extends ColoredStatusBarActivity {
         ButterKnife.bind(this);
 
         setupActionBar();
-
-        List = (ListView) findViewById(R.id.List);
-        new GetData().execute(PhpAPI.getProduit);
+        fetchProducts();
     }
 
     private void setupActionBar() {
@@ -56,70 +46,44 @@ public class ProductsActivity extends ColoredStatusBarActivity {
         mActionBar.setTitle("Produit");
     }
 
+    private void fetchProducts() {
+        sendHTTPRequest(PhpAPI.getProduit, null, Method.POST,
+                new RequestListener() {
+                    @Override
+                    public void onRequestSucceeded(JSONObject response, int status) {
+                        try {
+                            JSONArray listproduits = response.getJSONArray("produit");
+
+                            for (int i = 0; i < listproduits.length(); i++) {
+                                JSONObject usr = listproduits.getJSONObject(i);
+                                Product itm = new Product();
+                                itm.setID(Integer.parseInt(usr.getString("idp")));
+                                itm.setLibelle(usr.getString("libelle"));
+                                itm.setPrixTTC(Double.parseDouble(usr.getString("prixTTC")));
+                                itm.setQte(Integer.parseInt(usr.getString("qte")));
+                                itm.setPhoto(PhpAPI.IpBackend_IMAGES + usr.getString("photo"));
+
+                                ListProduit.add(itm);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        ProduitAdapter adapter = new ProduitAdapter(ProductsActivity.this, ListProduit);
+                        runOnUiThread(() -> productsListView.setAdapter(adapter));
+                    }
+
+                    @Override
+                    public void onRequestFailed() {
+                        runOnUiThread(() -> Toast.makeText(ProductsActivity.this,
+                                "No internet connection.",
+                                Toast.LENGTH_LONG).show());
+                    }
+                });
+    }
+
     @Override
     public void onAddPressed() {
         startActivity(new Intent(this, AddProductActivity.class));
-    }
-
-    private class GetData extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            try {
-                URL url = new URL(params[0]);
-                URLConnection conn = url.openConnection();
-                HttpURLConnection httpConn = (HttpURLConnection) conn;
-                httpConn.setAllowUserInteraction(false);
-                httpConn.setInstanceFollowRedirects(true);
-                httpConn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                Map<String, Object> Params = new LinkedHashMap<>();
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(new getQuery().getQuery(Params));
-                writer.flush();
-                writer.close();
-                os.close();
-                httpConn.connect();
-                InputStream is = httpConn.getInputStream();
-
-                return new convertInputStreamToString().convertInputStreamToString(is);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            System.out.println(s);
-
-            try {
-                JSONObject j = new JSONObject(s);
-                JSONArray listproduits = j.getJSONArray("produit");
-
-                for (int i = 0; i < listproduits.length(); i++) {
-                    JSONObject usr = listproduits.getJSONObject(i);
-                    Product itm = new Product();
-                    itm.setID(Integer.parseInt(usr.getString("idp")));
-                    itm.setLibelle(usr.getString("libelle"));
-                    itm.setPrixTTC(Double.parseDouble(usr.getString("prixTTC")));
-                    itm.setQte(Integer.parseInt(usr.getString("qte")));
-                    itm.setPhoto(PhpAPI.IpBackend_IMAGES + usr.getString("photo"));
-
-                    ListProduit.add(itm);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            ProduitAdapter adapter = new ProduitAdapter(ProductsActivity.this, ListProduit);
-            List.setAdapter(adapter);
-        }
     }
 }
