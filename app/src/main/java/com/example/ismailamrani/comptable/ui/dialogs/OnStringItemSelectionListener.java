972b8f886898faaca000b28b093a2f40b9e26989
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-27 16:29:22 +0100

    Remove unused source files.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/dialogs/OnStringItemSelectionListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.ismailamrani.comptable.ui.dialogs;
-
-/**
- * Created by Mohammed Aouf ZOUAG on 27/05/2016.
- */
-public interface OnStringItemSelectionListener {
-    void onStringItemSelected(String item);
-}
