4ae21ec48079876502aea6742b2140c1eb744b32
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-29 19:40:25 +0100

    SupplierDialog: display the informations of the selected supplier.

--- app/src/main/java/com/example/ismailamrani/comptable/ui/dialogs/SupplierDialog.java
+++ app/src/main/java/com/example/ismailamrani/comptable/ui/dialogs/SupplierDialog.java
@@ -5,15 +5,28 @@ import android.content.Context;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 import com.example.ismailamrani.comptable.R;
 import com.example.ismailamrani.comptable.models.Supplier;
 import com.example.ismailamrani.comptable.utils.CalculateScreenSize;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by Mohammed Aouf ZOUAG on 10/05/2016.
  */
 public class SupplierDialog extends Dialog {
+
+    @Bind(R.id.supplierTitleLabel)
+    TextView supplierTitleLabel;
+    @Bind(R.id.supplierAddressLabel)
+    TextView supplierAddressLabel;
+    @Bind(R.id.bottomAddressLabel)
+    TextView bottomAddressLabel;
+    @Bind(R.id.supplierPhoneLabel)
+    TextView supplierPhoneLabel;
 
     private Context mContext;
     private Supplier supplier;
@@ -21,7 +34,7 @@ public class SupplierDialog extends Dialog {
     public SupplierDialog(Context context, Supplier supplier) {
         super(context);
         mContext = context;
-        supplier = supplier;
+        this.supplier = supplier;
     }
 
     @Override
@@ -29,9 +42,10 @@ public class SupplierDialog extends Dialog {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.supplier_dialog);
+        ButterKnife.bind(this);
 
         setupWindow();
-        fetchSupplierDetails();
+        initializeUI();
     }
 
     private void setupWindow() {
@@ -40,7 +54,12 @@ public class SupplierDialog extends Dialog {
         getWindow().setLayout((int) (point.x - point.x * 0.1), (int) (point.y - point.y * 0.3));
     }
 
-    private void fetchSupplierDetails() {
+    private void initializeUI() {
+        String address = supplier.getAdresse();
 
+        supplierTitleLabel.setText(supplier.getNom());
+        supplierAddressLabel.setText(address);
+        bottomAddressLabel.setText(address);
+        supplierPhoneLabel.setText(supplier.getTel());
     }
 }
